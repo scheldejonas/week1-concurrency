9d32b4fe982124891227b785cef414d78241d70c
scheldejonas <scheldejonas@gmail.com>
2017-02-02 16:09:15 +0100

    Upgraded Thread Basic

--- demo/src/main/java/ThreadBasic.java
+++ demo/src/main/java/ThreadBasic.java
@@ -4,23 +4,27 @@
 public class ThreadBasic {
 
     public static void main(String[] args) {
+        for (int i = 0; i < 20; i++ ) {
+            startThreeThreadsWithSleep(i);
+        }
+    }
+
+    private static void startThreeThreadsWithSleep(int number) {
+
         Thread thread = new Thread() {
             @Override
             public void run() {
                 try {
+                    System.out.printf("Hi %s \n",number);
                     Thread.sleep(10000);
-                    System.out.println("Done sleeping");
+                    System.out.printf("Done sleeping %s \n", number);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         };
         thread.start();
-        System.out.println("Hi");
 
-        new Thread(() -> System.out.println("Hi1")).start();
-        new Thread(() -> System.out.println("Hi2")).start();
-        new Thread(() -> System.out.println("Hi3")).start();
     }
 
 
